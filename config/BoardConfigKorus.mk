a692151463076eecd63cf010de0b138efd0bbcf6
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    korus: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigKorus.mk
+++ config/BoardConfigKorus.mk
@@ -3,7 +3,6 @@ ifeq ($(WITH_KORUS_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.korus
 endif
 
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/korus/config/BoardConfigQcom.mk
 endif
