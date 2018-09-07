1b3f47a138ddff9ac419969c34d979a9774ce4f9
kongwoojin <kongwoojin03@gmail.com>
2018-09-07 18:17:20 +0900

    charger: Fix libhealthd
    Signed-off-by: kongwoojin <kongwoojin03@gmail.com>

--- charger/Android.mk
+++ charger/Android.mk
@@ -16,7 +16,7 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_lineage.cpp
-LOCAL_MODULE := libhealthd.lineage
+LOCAL_MODULE := libhealthd.korus
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := \
     system/core/healthd/include \
