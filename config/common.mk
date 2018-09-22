7f02438a030c7e41be20095b1c048363cc67d90e
kongwoojin <kongwoojin03@gmail.com>
2018-09-22 23:09:36 +0900

    common.mk: add some props
    Signed-off-by: kongwoojin <kongwoojin03@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -132,7 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AudioFX \
     LineageSettingsProvider \
-    LineageSetupWizard \
     Eleven \
     ExactCalculator \
     Jelly \
@@ -231,6 +230,26 @@ PRODUCT_PACKAGES_DEBUG += \
     procmem \
     procrank \
     strace
+
+# Additional props
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true \
+    ro.storage_manager.enabled=1 \
+    ro.opa.eligible_device=true \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    drm.service.enabled=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    keyguard.no_require_sim=true \
+    persist.sys.disable_rescue=true
 
 # Conditionally build in su
 ifneq ($(TARGET_BUILD_VARIANT),user)
